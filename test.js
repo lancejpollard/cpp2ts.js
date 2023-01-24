df7e85b29fb3240ca1638b47dff492ccf5960756
Lance Pollard <lp@elk.fm>
2023-01-23 18:18:36 -0800

    stuff

--- test.js
+++ test.js
@@ -3,24 +3,24 @@ const fs = require('fs')
 const convert = require('./src')
 const print = require('./src/print')
 
-fs.writeFileSync(
-  'fixtures/hyper.ast.json',
-  JSON.stringify(
-    print(fs.readFileSync('fixtures/hyper.h', 'utf-8')),
-    null,
-    2,
-  ),
-)
+if (!fs.existsSync('tmp')) {
+  fs.mkdirSync('tmp')
+}
+// fs.writeFileSync(
+//   'fixtures/hyper.ast.json',
+//   JSON.stringify(
+//     print(fs.readFileSync('fixtures/hyper.h', 'utf-8')),
+//     null,
+//     2,
+//   ),
+// )
 
-// if (!fs.existsSync('tmp')) {
-//   fs.mkdirSync('tmp')
-// }
-// // fs.writeFileSync('hyperpoint.2.json', JSON.stringify(traverse(ast), null, 2))
-// fs.writeFileSync(
-//   'tmp/hyperpoint.ts',
-//   convert(fs.readFileSync('fixtures/hyperpoint.cpp', 'utf-8')),
-// )
-// fs.writeFileSync(
-//   'tmp/hyper.ts',
-//   convert(fs.readFileSync('fixtures/hyper.h', 'utf-8')),
-// )
+// fs.writeFileSync('hyperpoint.2.json', JSON.stringify(traverse(ast), null, 2))
+fs.writeFileSync(
+  'tmp/hyperpoint.ts',
+  convert(fs.readFileSync('fixtures/hyperpoint.cpp', 'utf-8')),
+)
+fs.writeFileSync(
+  'tmp/hyper.ts',
+  convert(fs.readFileSync('fixtures/hyper.h', 'utf-8')),
+)
